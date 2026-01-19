8a05b9f3a923e3e3d0d82b93ff5ec786dc28eecd
debboutr <debboutr@gmail.com>
2026-01-19 13:07:11 -0800

    add static and media url

--- core/settings.py
+++ core/settings.py
@@ -115,6 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/vol/static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/vol/media/'
+
+CSRF_TRUSTED_ORIGINS = ["https://debbout.info","https://www.debbout.info"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
